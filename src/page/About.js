570af44d8749d011e09978166589a034079bfb0c
Andrey Zhukov <zhukov290791@gmail.com>
2020-03-12 09:59:36 +0500

    first commit

---
+++ src/page/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Fluid jumbotron</h1>
+                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cumque distinctio earum eveniet excepturi impedit in ipsa, itaque magnam minima, molestiae necessitatibus nulla obcaecati odio pariatur quaerat, quidem quo repellat repellendus repudiandae temporibus totam voluptatibus voluptatum. A consequuntur doloremque explicabo ipsa maxime mollitia natus odit quasi quos suscipit. A alias deleniti ducimus enim esse eum impedit ipsa maxime odio, optio praesentium provident quod, sapiente. A cumque deleniti dolores eaque, eligendi eum ex, excepturi fuga ipsam iure labore minima natus officiis provident quae quidem quis ratione suscipit totam vero! Accusantium debitis ducimus itaque labore maxime non officia recusandae reprehenderit similique voluptates.</p>
+            </div>
+        </div>
+    )
+};
